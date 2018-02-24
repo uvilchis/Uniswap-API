09cf14ae8bb86ba135657fc403bd053da06764f8
Uciel <uciel777@Uciels-MBP.home>
2018-02-24 12:44:24 -0500

    graphql event mutations and timestamp queries working

--- src/api/resources/event/event.model.js
+++ src/api/resources/event/event.model.js
@@ -1,17 +1,14 @@
 import mongoose from 'mongoose';
 
-
-// token name or token ID? 
 const eventSchema = new mongoose.Schema({
-    tokenName: String, 
-    ethInMarket: Number,
-    tokensInMarket: Number,
-    invariant: Number, 
-    ethValueOfToken: Number
+    symbol: String, 
+    ethInMarket: String,
+    tokensInMarket: String,
+    invariant: String, 
+    ethValueOfToken: String
 }, { timestamps: true })
 
-// events will need to be 
-// added (uniquely)
-// retrieve - all events retrieved by token 
-
 export const Event = mongoose.model('event', eventSchema);
+
+
+  
