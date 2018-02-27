22b521e54182d0c855e0af05ab3b4daa4ee9a271
Uciel <uciel777@Uciels-MBP.home>
2018-02-26 21:12:42 -0500

    cleaned up middleware functions

--- src/listeners/tokens/index.js
+++ src/listeners/tokens/index.js
@@ -1,2 +1,13 @@
 export * from './UNI'
 export * from './SWT'
+
+export const getTokenCost = (tokenDecimals, ethInMarket, tokensInMarket, invariant) => {
+    let ethInDecimals = 0.01;
+    let ethInWei = ethInDecimals*10**18;
+    let fee = ethInWei/500;
+    let newEthInMarket = +ethInMarket + ethInWei - fee;
+    let newTokensInMarket = +invariant/newEthInMarket;
+    let tokensOut = +tokensInMarket - newTokensInMarket;
+    let tokensOutDecimals = tokensOut/tokenDecimals;
+    return JSON.stringify(tokensOutDecimals/ethInDecimals)
+}
