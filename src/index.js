3330437f6c8818575173cb922db8d9338e987ab6
Uciel <uciel777@new-host-2.home>
2018-03-25 23:51:28 -0400

    dynamically generate methods and intervals for new tokens

--- src/index.js
+++ src/index.js
@@ -19,3 +19,54 @@ if (module.hot) {
 	currentApp = app
   })
 }
+
+/* 
+The current workflow for this project is as follows 
+a) Two MongoDB Models (/Resources/event.model & token.model)
+- one for adding a new token, the other for adding events 
+- Mongo's query functions are written around the structure of these models 
+
+b) Resolvers (/Resources/event.resolvers & token.resolvers)
+- fire upon a GraphQL query
+- change how info is pulled from DB in these functions 
+
+c) GraphQL Models (/Resources/event.graphql & token.graphql)
+- as similar as possible in structure to mongo model
+- type: defines the initial query structure 
+- input: same structure as type, this is how graphQL will expect inputs for this type
+- NOTE: Exclamation points mean that field is REQUIRED. Will throw errors if not provided
+- extend type Query and extend type Mutation are extending the baseSchema in /Resources/graphQLRouter
+
+
+d) GraphQL Router 
+- Is where the GraphQL Models and Resolvers are compiled together by graphql-tools makeExecutableSchema
+- tokenType and eventType are both the GraphQL Models 
+- Query and Mutation are native to GraphQL
+- ^^^ their use is instantiated here and extended by the models you create 
+
+Everything above is scalable - the functions are token/event agnostic and will continue to work as more are added 
+The listeners however, are a different story 
+e) /Listeners/Tokens 
+- functions are being written for every token 
+- UNI has its own function set, SWT has its own set 
+- these functions all do the same thing - the only thing that changes are the token and exchange addresses 
+- presumably, you'd write one of these for each new token added - not a scalable solution 
+- this is not a scalable solution 
+
+THE WAY IT WORKS NOW 
+- we create a new file in listeners/tokens/TOKENSYMBOL
+- these functions are imported into middleware.js 
+- ANOTHER function is created, which fires a set of functions in order 
+- then manually writes the symbol for this token at the function's end 
+- these functions are separately imported into server.js and individually invoked 
+
+
+THE MISSION
+- write a function factory of sorts 
+- takes an array of arrays structured like so => ['symbol', 'address']
+- create the functions for each of those arrays, then run them
+
+TO ADD NEW OFFICIALLY SUPPORTED TOKENS 
+- /listeners/tokens/symbolsAndAddresses
+- 0th index = symbol, 1st index = exchangeAddress 
+*/
--- src/middleware.js
+++ src/middleware.js
@@ -8,47 +8,18 @@ export const setGlobalMiddleware = (app) => {
   app.use(bodyParser.json());
 }
 
-export const uniEventListener = () => {
-  setInterval(async() => {
-    let uniDecimals = listeners.uniDecimals;
-    let ethInMarket = await listeners.getUniMarketEth();
-    let tokensInMarket = await listeners.getUniMarketTokens();
-    let invariant = await listeners.getUniInvariant();
-    let ethValueOfToken = listeners.getTokenCost(uniDecimals, ethInMarket, tokensInMarket, invariant);
-    post.postToEvents("UNI", ethInMarket, tokensInMarket, invariant, ethValueOfToken);
-  }, 15000)
+const contractsAndMethods = listeners.contractAndMethodCreator();
+// you now want to create a listener for each of the functions 
+export const createListeners = () => {
+  for (let symbol in contractsAndMethods) {
+    let token = contractsAndMethods[symbol];
+    setInterval(async() => {
+      let decimals = 10**18;
+      let ethPool = await token.getEthPool();
+      let tokenPool = await token.getTokenPool();
+      let invariant = await token.getInvariant();
+      let ethValueOfToken = listeners.getTokenCost(decimals, ethPool, tokenPool, invariant);
+      post.postToEvents(symbol, ethPool, tokenPool, invariant, ethValueOfToken);
+    }, 3000);
+  }
 }
-
-export const swtEventListener = () => {
-  setInterval(async() => {
-    let swtDecimals = listeners.swtDecimals;
-    let ethInMarket = await listeners.getSwtMarketEth();
-    let tokensInMarket = await listeners.getSwtMarketTokens();
-    let invariant = await listeners.getSwtInvariant();  
-    let ethValueOfToken = listeners.getTokenCost(swtDecimals, ethInMarket, tokensInMarket, invariant);
-    post.postToEvents("SWT", ethInMarket, tokensInMarket, invariant, ethValueOfToken);
-  }, 15000)
-}
-
-// not a scalable solution
-// as more tokens get added, the chance of an event being added at exactly the 15 second mark decreases 
-// need to revisit this and come up with a better solution for ensuring data is written to database at exactly the 15 second timestamp 
-
-export const startEventListeners = () => {
-  let hasHit0thSecond = false;
-   let secondChecker = setInterval (()=> {
-      let currentSecond = new Date().getSeconds();
-      console.log(hasHit0thSecond, currentSecond)
-      if (currentSecond === 0) {
-        uniEventListener();
-        swtEventListener();
-        hasHit0thSecond = true;
-      }
-      if (hasHit0thSecond) {
-        clearInterval(secondChecker);
-        console.log('listeners starting');
-      }
-    }, 100)
-}
-
-
